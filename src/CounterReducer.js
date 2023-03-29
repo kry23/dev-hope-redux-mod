13eb96cbcc75e140065a3a5f8214658c5ac6a285
kry23 <infest.redstar@gmail.com>
2023-03-29 16:21:23 +0300

    redux-1 sol.

--- src/CounterReducer.js
+++ src/CounterReducer.js
@@ -1,36 +1,44 @@
-
-const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
-const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
-const RESET_COUNTER = 'RESET_COUNTER';
-
-
-const incrementCounter = () => ({ type: INCREMENT_COUNTER });
-const decrementCounter = () => ({ type: DECREMENT_COUNTER });
-const resetCounter = () => ({ type: RESET_COUNTER });
-
-
+const INCREMENT_COUNTER = "INCREMENT_COUNTER";
+const DECREMENT_COUNTER = "DECREMENT_COUNTER";
+const RESET_COUNTER = "RESET_COUNTER";
 const initialState = {
-  count: 0
+  count: 0,
 };
 
-const counterReducer = (state = initialState, action) => {
+export const incrementCounter = (by = 1) => {
+  return {
+    type: INCREMENT_COUNTER,
+    payload: by,
+  };
+};
+export const decrementCounter = (by = 1) => {
+  return {
+    type: DECREMENT_COUNTER,
+    payload: by,
+  };
+};
+export const resetCounter = () => {
+  return {
+    type: RESET_COUNTER,
+  };
+};
+
+export const counterReducer = (state = initialState, action) => {
   switch (action.type) {
-    case INCREMENT_COUNTER:
-      return {
-        ...state,
-        count: state.count + 1
-      };
-    case DECREMENT_COUNTER:
-      return {
-        ...state,
-        count: state.count - 1
-      };
-    case RESET_COUNTER:
-      return {
-        ...state,
-        count: 0
-      };
-    default:
+    case INCREMENT_COUNTER: {
+      return state + action.payload;
+    }
+
+    case DECREMENT_COUNTER: {
+      return state + action.payload;
+    }
+
+    case RESET_COUNTER: {
+      return initialState;
+    }
+
+    default: {
       return state;
+    }
   }
 };
